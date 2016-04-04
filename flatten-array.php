ca9f04ff071d0ec00ede03d41c697e191b4146f8
Max Zhuk <zhukmax@ya.ru>
2016-04-04 14:14:01 +0300

    Create flatten-array.php

---
+++ flatten-array.php
@@ -0,0 +1,20 @@
+<?php
+trait FlattenTrait {
+  public function __construct()
+  {
+    $this->output = array();
+  }
+  public function addItem ($item)
+  {
+    $this->output[] = $item;
+  }
+  public function flatten ($input)
+  {
+    $this->output = array();
+    if (!is_array($input)) return $input;
+  
+    array_walk_recursive($input, 'addItem');
+    return $this->output;
+  }
+}
+?>
